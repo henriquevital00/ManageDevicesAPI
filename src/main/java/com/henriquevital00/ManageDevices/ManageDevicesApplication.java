ed620bcd1bf240c149ea3ce43b3606cc72f0ee22
henriquevital00 <henrique.carvalho1000@hotmail.com>
2025-03-26 22:25:29 -0300

    Created docker config and java application

---
+++ src/main/java/com/henriquevital00/ManageDevices/ManageDevicesApplication.java
@@ -0,0 +1,13 @@
+package com.henriquevital00.ManageDevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriquevital00/ManageDevices/ManageDevicesApplicationTests.java
@@ -0,0 +1,10 @@
+package com.henriquevital00.ManageDevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageDevicesApplicationTests {
+
+
+}
