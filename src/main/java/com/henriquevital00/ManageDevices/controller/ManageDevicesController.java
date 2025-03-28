69f28cbadf9c7cdbef208e34660d2c64379bf1ee
henriquevital00 <henrique.carvalho1000@hotmail.com>
2025-03-28 15:16:39 -0300

    Updated documentation

--- src/main/java/com/henriquevital00/ManageDevices/controller/ManageDevicesController.java
+++ src/main/java/com/henriquevital00/ManageDevices/controller/ManageDevicesController.java
@@ -2,8 +2,14 @@ package com.henriquevital00.ManageDevices.controller;
 
 import com.henriquevital00.ManageDevices.domain.dto.DeviceCreateDto;
 import com.henriquevital00.ManageDevices.domain.dto.DeviceDto;
+import com.henriquevital00.ManageDevices.domain.dto.ErrorResponseDto;
 import com.henriquevital00.ManageDevices.domain.enums.DeviceStateEnum;
 import com.henriquevital00.ManageDevices.services.DeviceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -27,7 +33,28 @@ import java.util.List;
 public class ManageDevicesController {
     private DeviceService deviceService;
 
-
+    @Operation(
+            summary = "Create Device REST API",
+            description = "REST API to create new Device"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "HTTP Status Bad Request"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/createDevice")
     public ResponseEntity<DeviceDto> createDevice(@Valid @RequestBody DeviceCreateDto device) {
         DeviceDto createdDevice = deviceService.createDevice(device);
@@ -36,6 +63,23 @@ public class ManageDevicesController {
                 .body(createdDevice);
     }
 
+    @Operation(
+            summary = "Fetch All Devices REST API",
+            description = "REST API to Fetch all Devices data"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/devices")
     public ResponseEntity<List<DeviceDto>> getAllDevices(
             @RequestParam(defaultValue = "0") int page,
@@ -45,12 +89,60 @@ public class ManageDevicesController {
         return ResponseEntity.ok(devices);
     }
 
+    @Operation(
+            summary = "Fetch Device by ID REST API",
+            description = "REST API to Fetch a Device by ID"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/device/{id}")
     public ResponseEntity<DeviceDto> getDeviceById(@PathVariable Long id) {
         DeviceDto device = deviceService.getDeviceById(id);
         return ResponseEntity.ok(device);
     }
 
+    @Operation(
+            summary = "Fetch Devices by Brand REST API",
+            description = "REST API to Fetch Devices by Brand"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/devices/brand")
     public ResponseEntity<List<DeviceDto>> getDevicesByBrand(@RequestParam String brand,
                                                              @RequestParam(defaultValue = "0") int page,
@@ -59,6 +151,30 @@ public class ManageDevicesController {
         return ResponseEntity.ok(devices);
     }
 
+    @Operation(
+            summary = "Fetch Devices by State REST API",
+            description = "REST API to Fetch Devices by State"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/devices/state")
     public ResponseEntity<List<DeviceDto>> getDevicesByState(@RequestParam DeviceStateEnum state,
                                                              @RequestParam(defaultValue = "0") int page,
@@ -67,12 +183,60 @@ public class ManageDevicesController {
         return ResponseEntity.ok(devices);
     }
 
+    @Operation(
+            summary = "Update Device REST API",
+            description = "REST API to Update a Device"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PutMapping("/device/{id}")
     public ResponseEntity<DeviceDto> updateDevice(@PathVariable Long id, @Valid @RequestBody DeviceCreateDto device) {
         DeviceDto updatedDevice = deviceService.updateDevice(id, device);
         return ResponseEntity.ok(updatedDevice);
     }
 
+    @Operation(
+            summary = "Delete Device REST API",
+            description = "REST API to Delete a Device"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "HTTP Status No Content"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/device/{id}")
     public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
         deviceService.deleteDevice(id);
